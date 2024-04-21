430998f025a2635e9b30563a6cede809113df39f
Anton <anton22582258@gmail.com>
2024-04-21 16:55:08 +0300

    Homework4-1

---
+++ src/main/java/ru/hogwarts/school5/School5Application.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class School5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(School5Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hogwarts/school5/School5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class School5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
